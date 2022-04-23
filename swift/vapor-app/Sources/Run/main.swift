8b4579afeaaa95389395c8a5dec389e795468692
Kevin Wang <kwangsan@gmail.com>
2022-04-23 00:12:45 -0400

    fix(swift)

--- swift/vapor-app/Sources/Run/main.swift
+++ swift/vapor-app/Sources/Run/main.swift
@@ -5,8 +5,8 @@ var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 
-// let port = Environment.get("PORT") ?? "8080"
-// app.http.server.configuration.port = Int(port)!
+let port = Environment.get("PORT") ?? "8080"
+app.http.server.configuration.port = Int(port)!
 
 defer { app.shutdown() }
 try configure(app)
