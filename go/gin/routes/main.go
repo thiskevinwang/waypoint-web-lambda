a62a62df9acd0294b045d91c0155b9ece243c90a
Kevin Wang <kwangsan@gmail.com>
2022-08-14 15:32:57 -0400

    comment out db connection

--- go/gin/routes/main.go
+++ go/gin/routes/main.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"main/controllers"
-	"main/middleware"
+	// "main/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +10,10 @@ import (
 // the routes and handlers are registered on the base path (/)
 func CreateRoutes(rg *gin.RouterGroup) {
 	rg.GET("/ping", controllers.Ping)
-	rg.GET("/health", controllers.HealthCheck)
+	// rg.GET("/health", controllers.HealthCheck)
 
-	weights := rg.Group("/weights")
-	weights.Use(middleware.RequireAuth)
-	weights.POST("/", controllers.WriteWeights)
-	weights.GET("/:batch_id", controllers.ListWeights)
+	// weights := rg.Group("/weights")
+	// weights.Use(middleware.RequireAuth)
+	// weights.POST("/", controllers.WriteWeights)
+	// weights.GET("/:batch_id", controllers.ListWeights)
 }
